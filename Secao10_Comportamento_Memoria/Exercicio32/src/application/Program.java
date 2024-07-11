6231d8cb247cbc75ecb9545b76583c439ca2e2f2
HugoTosi <hugotosi42@gmail.com>
2024-07-11 20:18:00 -0300

    Nova resolucao do exercicio 32

--- Secao10_Comportamento_Memoria/Exercicio32/src/application/Program.java
+++ Secao10_Comportamento_Memoria/Exercicio32/src/application/Program.java
@@ -4,7 +4,7 @@ bem como os nomes dessas pessoas caso houver. */
 
 package application;
 
-import entities.Person;
+import entities.Individual;
 
 import java.util.Locale;
 import java.util.Scanner;
@@ -14,46 +14,55 @@ public class Program {
 
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
+        int contador = 0;
+        double soma = 0, media = 0;
 
-        int n;
-        double mediaAltura = 0, porcentagem = 0, sub = 0;
+        System.out.println("Quantas pessoas serão digitadas?");
+        int n = sc.nextInt();
+        Individual[] vect = new Individual[n];
 
-        System.out.println("Quantas pessoas serao digitadas?");
-        n = sc.nextInt();
-        Person[] vetor = new Person[n];
-
-        for (int i = 0; i < vetor.length; i++) {
+        for (int i = 0; i < vect.length; i++) {
+            System.out.println("Dados da " + (i + 1) + "ª pessoa:\n");
+            vect[i] = new Individual();
             sc.nextLine();
-            System.out.println("Digite os dados da " + (i + 1) +"a pessoa:");
-            System.out.print("Nome: ");
-            String nome = sc.nextLine();
-            System.out.print("Idade: ");
-            int idade = sc.nextInt();
-            System.out.print("Altura: ");
-            double altura = sc.nextDouble();
-            vetor[i] = new Person(nome, idade, altura);
-        }
-        for (int i = 0; i < vetor.length; i++) {
-            mediaAltura += vetor[i].getHeight() / n;
+            System.out.print("Nome:");
+            vect[i].setNome(sc.nextLine());
+            System.out.print("Idade:");
+            vect[i].setIdade(sc.nextInt());
+            System.out.print("Altura:");
+            vect[i].setAltura(sc.nextDouble());
 
-        }
-
-        for (int i = 0; i < vetor.length; i++) {
-            if (vetor[i].getAge() < 16) {
-                sub += 1;
-                porcentagem = sub * 100 / n;
-
-
+            if (vect[i].getIdade() > 16) {
+                contador += 1;
             }
         }
-        System.out.println("Altura média:" + mediaAltura);
-        System.out.println("Pessoas com menos de 16 anos: " + porcentagem + "%");
 
-        for (int i = 0; i < vetor.length; i++) {
-            if (vetor[i].getAge() < 16) {
-                System.out.println(vetor[i].getName());
+        Individual[] qtdMenores = new Individual[contador];
+        contador = 0;
+        for (int i = 0; i < vect.length; i++) {
+            soma += vect[i].getAltura();
+
+            if (vect[i].getIdade() < 16) {
+                qtdMenores[contador++] = vect[i];
             }
+
+        }
+        media = soma / vect.length;
+        double porcentagem = (double) qtdMenores.length * 100 / vect.length;
+
+        System.out.printf("\nAltura média: %.2f", media);
+        System.out.printf("\nPessoas com menos de 16 anos: %.0f%%\n", porcentagem);
+
+
+        if (contador > 0) {
+
+            for (int i = 0; i < qtdMenores.length; i++) {
+                System.out.println(qtdMenores[i].toString());
+            }
+        } else {
+            System.out.println("Não existem menores de 16 anos no grupo informado.");
+        }
+
 
     }
-}
 }
