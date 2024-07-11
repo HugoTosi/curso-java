af140df58522411121f32a6f8940d6fa43913be6
HugoTosi <hugotosi42@gmail.com>
2024-07-11 20:19:39 -0300

    Nova resolucao do exercicio 32

---
+++ Secao10_Comportamento_Memoria/Exercicio32/src/entities/Individual.java
@@ -0,0 +1,45 @@
+package entities;
+
+public class Individual {
+
+    private String nome;
+    private int idade;
+    private double altura;
+
+    public Individual() {
+    }
+
+    public Individual(String nome, int idade, double altura) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public String toString(){
+        return "Nome:" + nome;
+    }
+}
